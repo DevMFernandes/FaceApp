7dfbdf4661d5d8df9716015c5de6ad96a36c9f73
DevMFernandes <contato@marcos-fernandes.com>
2019-07-25 11:35:21 -0300

    testings for user controllers

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,14 +1,32 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get user_show_url
-    assert_response :success
-  end
 
-  test "should get index" do
-    get user_index_url
-    assert_response :success
-  end
+  # def setup
+  #   @user = users(:michael)
+  #   @other_user = users(:archer)
+  # end
+
+  # test "should redirect index when not logged in" do
+  #   get users_path
+  #   assert_redirected_to login_url
+  # end
+
+  # test "should redirect edit when logged in as wrong user" do
+  #   log_in_as(@other_user)
+  #   get edit_user_path(@user)
+  #   assert flash.empty?
+  #   assert_redirected_to root_url
+  # end
+
+  # test "should get show" do
+  #   get user_show_url
+  #   assert_response :success
+  # end
+
+  # test "should get index" do
+  #   get user_index_url
+  #   assert_response :success
+  # end
 
 end
