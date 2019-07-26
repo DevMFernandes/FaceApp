8bf3471190c5878432379c24cad01214f4ca5da3
DevMFernandes <contato@marcos-fernandes.com>
2019-07-26 17:51:33 -0300

    generated comments controller, added comments to post page, added form to comment, failed to comment using the form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to @post, notice: 'Comment was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:body).merge(user_id: current_user.id)
+    end
+end
