8b705d2bb17c0d1ce97e19e20deca63144ad25e3
erib <eri-b@users.noreply.github.com>
2019-07-30 14:31:45 -0400

    updated redirects for likes , add misc stuff

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,7 +12,7 @@ class LikesController < ApplicationController
 
       @like.user_id = current_user.id
       if @like.save
-        redirect_to posts_path, notice: 'I just liked something'
+        redirect_to posts_path
       else
         redirect_to posts_path, notice: 'Like failed'
       end
@@ -25,7 +25,7 @@ class LikesController < ApplicationController
 
       @like.user_id = current_user.id
       if @like.save
-        redirect_to @post, notice: 'I just liked something'
+        redirect_to @post
       else
         redirect_to @post, notice: 'Like failed'
       end
@@ -39,7 +39,12 @@ class LikesController < ApplicationController
 
   def destroy
     @like.destroy
-    redirect_to posts_path
+    if params[:like_type] == "Post"
+      redirect_to posts_path
+    elsif params[:like_type] == "Comment"
+      @post = Post.find(params[:post_id])
+      redirect_to @post
+    end
   end
 
   private
@@ -49,7 +54,7 @@ class LikesController < ApplicationController
     end
 
     def already_liked?
-      if params[:like_type] == "Post" 
+      if params[:like_type] == "Post"
           redirect_to posts_path, notice: 'Already liked post!' if like_exist?
       elsif params[:like_type] == "Comment"
           redirect_to posts_path, notice: 'Already liked comment!' if like_exist?
