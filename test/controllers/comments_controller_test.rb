00a13f87aee016f65c722212194fadb766ece643
erib <eri-b@users.noreply.github.com>
2019-07-30 14:52:15 -0400

    small updates to layout, an attempt at testing

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @post = posts(:one)
+    @user = users(:michael)
+  end
+
   test "should get create" do
-    get comments_create_url
+    get  new_post_comment_url(@post)
     assert_response :success
   end
 
-  test "should get destroy" do
-    get comments_destroy_url
-    assert_response :success
-  end
+  # test "should get destroy" do
+  #   get post_comment_url
+  #   assert_response :success
+  # end
 
 end
