8f7dfee201c0458eb31e3ad03407b1070b05b5b7
DevMFernandes <contato@marcos-fernandes.com>
2019-07-30 15:08:15 -0300

    refactor of likes controller, likes for posts redirects, likes for comments redirects, gotta check for unlike on comments redirect

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,25 +6,33 @@ class LikesController < ApplicationController
   def create
 
     # Like Post
-    if params[:post_id]
-      @post = Post.find(params[:post_id])
+    if params[:like_type] == "Post"
+      @post = Post.find(params[:likeable_id])
       @like = @post.likes.build
 
+      @like.user_id = current_user.id
+      if @like.save
+        redirect_to posts_path, notice: 'I just liked something'
+      else
+        redirect_to posts_path, notice: 'Like failed'
+      end
+
     # Like Comment
-    elsif params[:comment_id]
-      @comment = Comment.find(params[:comment_id])
+    elsif params[:like_type] == "Comment"
+      @post = Post.find(params[:post_id])
+      @comment = Comment.find(params[:likeable_id])
       @like = @comment.likes.build
+
+      @like.user_id = current_user.id
+      if @like.save
+        redirect_to @post, notice: 'I just liked something'
+      else
+        redirect_to @post, notice: 'Like failed'
+      end
 
     # If anything goes wrong
     else
-      redirect_to posts_path, notice: 'the if statement failed'
-    end
-
-    @like.user_id = current_user.id
-    if @like.save
-      redirect_to posts_path, notice: 'I just liked something'
-    else
-      redirect_to posts_path, notice: 'Like failed'
+      redirect_to :root, notice: 'the if statement failed'
     end
 
   end
@@ -36,37 +44,28 @@ class LikesController < ApplicationController
 
   private
 
-    def already_liked?
-      if params[:post_id]
-        if Like.where(user_id: current_user.id, likeable_type: "Post", likeable_id: params[:post_id]).exists?
-          redirect_to posts_path, notice: 'Already liked post!'
-        end
-      elsif params[:comment_id]
-        if Like.where(user_id: current_user.id, likeable_type: "Comment", likeable_id: params[:comment_id]).exists?
-          redirect_to posts_path, notice: 'Already liked comment!'
-        end
-      end
+    def like_exist?
+      Like.where(user_id: current_user.id, likeable_type: params[:like_type], likeable_id: params[:likeable_id]).exists?
+    end
 
+    def already_liked?
+      if params[:like_type] == "Post" 
+          redirect_to posts_path, notice: 'Already liked post!' if like_exist?
+      elsif params[:like_type] == "Comment"
+          redirect_to posts_path, notice: 'Already liked comment!' if like_exist?
+      end
     end
 
     def not_liked?
-      if params[:post_id]
-        if !Like.where(user_id: current_user.id, likeable_type: "Post", likeable_id: params[:post_id]).exists?
-          redirect_to posts_path, notice: 'Already unliked comment!'
-        end
-      elsif params[:comment_id]
-        if !Like.where(user_id: current_user.id, likeable_type: "Comment", likeable_id: params[:comment_id]).exists?
-          redirect_to posts_path, notice: 'Already unliked comment!'
-        end
+      if params[:like_type] == "Post"
+          redirect_to posts_path, notice: 'Already unliked comment!' if !like_exist?
+      elsif params[:like_type] == "Comment"
+          redirect_to posts_path, notice: 'Already unliked comment!' if !like_exist?
       end
     end
 
     def find_like
-      if params[:post_id]
-        @like = Like.find_by(likeable_id: params[:post_id], likeable_type: "Post", user_id: current_user.id)
-      elsif params[:comment_id]
-        @like = Like.find_by(likeable_id: params[:comment_id], likeable_type: "Comment", user_id: current_user.id)
-      end
+      @like = Like.find_by(likeable_id: params[:likeable_id], likeable_type: params[:like_type], user_id: current_user.id)
     end
 
 end
