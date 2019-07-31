5613e5614efde9d9f0c57264e11d61581bba4153
DevMFernandes <contato@marcos-fernandes.com>
2019-07-31 19:04:00 -0300

    database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,20 @@
 User.create(:name => 'Eric', :email => 'eric@mail.com', :password => 'password', :location_id => rand(1..20))
 
 30.times do
-    User.create(:name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password', :location_id => rand(1..20))
+    User.create(:name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password', :location_id => rand(1..30))
     Location.create(:city => Faker::Address.city, :country => Faker::Address.country)
 end
 
-(2..10).each do |i|
-    Friendship.create(:creator_id => 1,:recipient_id => i, :status => true)
+(1..15).each do |j|
+
+    (1..10).each do |i|
+        Friendship.create(:creator_id => j,:recipient_id => i+1, :status => true)
+    end
 end
 
-(11..15).each do |i|
-    Friendship.create(:creator_id => 1,:recipient_id => i, :status => false)
-end
+(1..15).each do |j|
 
-(16..18).each do |i|
-    Friendship.create(:creator_id => i,:recipient_id => 1, :status => true)
-end
-
-(19..20).each do |i|
-    Friendship.create(:creator_id => i,:recipient_id => 1, :status => false)
+    (15..20).each do |i|
+        Friendship.create(:creator_id => i,:recipient_id => j, :status => false)
+    end
 end
