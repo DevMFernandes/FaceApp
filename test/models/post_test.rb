b91f6d837b6e4d8130532426f7bbbcd3a1479f2d
DevMFernandes <contato@marcos-fernandes.com>
2019-08-14 16:02:40 -0300

    posts and commits tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,7 +2,34 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   include Devise::Test::IntegrationHelpers
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:michael)
+    @post = @user.posts.build(body: "Lorem ipsum")
+    @post2 = posts(:one)
+  end
+  
+  test "user post should count to user posts" do
+    user = User.create(email: "michdsfsdfdsael@example.com", name: "michael", password: "foobar", password_confirmation: "foobar")
+    assert_difference 'user.posts.count', 1 do
+      user.posts.create!(body: "Lorem ipsum")
+    end
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "destroying a post should count -1 to Posts" do
+    assert_difference 'Post.count', -1 do
+      @post2.destroy
+    end
+  end
+
+  test "post should not save without body" do
+    post = Post.new(user_id: 1)
+    assert_not post.save
+  end
+
 end
